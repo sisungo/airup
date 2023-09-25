8581198804f19dc54d38a9b0e17c68c4e06d385f
sisungo <sisungo@pworks-laptop.local>
2023-09-25 22:52:55 +0800

    [Feature] Logging API for ACE

--- airupfx/src/ace/builtins.rs
+++ airupfx/src/ace/builtins.rs
@@ -1,17 +1,33 @@
 use crate::{process::ExitStatus, signal::SIGTERM};
-use std::{collections::HashMap, hash::BuildHasher};
+use std::{collections::HashMap, hash::BuildHasher, future::Future};
 use tokio::sync::mpsc;
 
 pub type BuiltinModule = fn(args: Vec<String>) -> mpsc::Receiver<i32>;
 
 pub fn init<H: BuildHasher>(builtins: &mut HashMap<&'static str, BuiltinModule, H>) {
     builtins.insert("noop", noop);
+    builtins.insert("console.info", console_info);
+    builtins.insert("console.warn", console_warn);
+    builtins.insert("console.error", console_error);
+}
+
+pub fn console_info(args: Vec<String>) -> mpsc::Receiver<i32> {
+    tracing::info!(target: "console", "{}", merge_args(&args));
+    builtin_impl(async { 0 })
+}
+
+pub fn console_warn(args: Vec<String>) -> mpsc::Receiver<i32> {
+    tracing::warn!(target: "console", "{}", merge_args(&args));
+    builtin_impl(async { 0 })
+}
+
+pub fn console_error(args: Vec<String>) -> mpsc::Receiver<i32> {
+    tracing::error!(target: "console", "{}", merge_args(&args));
+    builtin_impl(async { 0 })
 }
 
 pub fn noop(_: Vec<String>) -> mpsc::Receiver<i32> {
-    let (tx, rx) = mpsc::channel(1);
-    tokio::spawn(ret(tx, 0));
-    rx
+    builtin_impl(async { 0 })
 }
 
 pub async fn wait(rx: &mut mpsc::Receiver<i32>) -> ExitStatus {
@@ -21,6 +37,24 @@ pub async fn wait(rx: &mut mpsc::Receiver<i32>) -> ExitStatus {
     }
 }
 
+fn builtin_impl<F: Future<Output = i32> + Send + Sync + 'static>(future: F) -> mpsc::Receiver<i32> {
+    let (tx, rx) = mpsc::channel(1);
+    tokio::spawn(async move {
+        ret(tx, future.await).await;
+    });
+    rx
+}
+
 async fn ret(tx: mpsc::Sender<i32>, val: i32) {
     while let Ok(_) = tx.send(val).await {}
+}
+
+fn merge_args(args: &[String]) -> String {
+    let mut result = String::with_capacity(args.len() * 12);
+    for arg in args {
+        result.push_str(arg);
+        result.push(' ');
+    }
+    result.pop();
+    result
 }
